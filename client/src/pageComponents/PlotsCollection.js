989366522cb9fc8b5035790a2b24c41382876250
sprea22 <a.spreafico13@campus.unimib.it>
2020-11-25 09:35:40 +0100

    Fix typo in Plots collection tab

--- client/src/pageComponents/PlotsCollection.js
+++ client/src/pageComponents/PlotsCollection.js
@@ -57,8 +57,8 @@ class PlotsCollection extends Component {
                                     <li> <b> Data Freq: </b> monthly </li>
                                     <li> <b> Year: </b> {myJson[key]["year"]} </li>
                                     <li> <b> Location: </b> {myJson[key]["geo"]} </li>
-                                    <li> <b> Unit of Measure: </b> {myJson[key]["unit_of_measure"]} s</li>
-                                    <li> <b> Source: </b> <url> https://www150.statcan.gc.ca </url></li>
+                                    <li> <b> Unit of Measure: </b> {myJson[key]["unit_of_measure"]}</li>
+                                <b> Source: </b> <url> https://www150.statcan.gc.ca </url>
                                     </div>
                                 </Col>
                             </Row>
